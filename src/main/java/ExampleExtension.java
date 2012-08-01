bb0fca629838220f329d7c0bf5fb007a2bc81e2d
Ståle W. Pedersen <stalep@gmail.com>
2012-08-01 23:09:03 +0200

    initial commit for extensions. testing out man...

---
+++ src/main/java/ExampleExtension.java
@@ -0,0 +1,138 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jboss.jreadline.complete.CompleteOperation;
+import org.jboss.jreadline.complete.Completion;
+import org.jboss.jreadline.console.Console;
+import org.jboss.jreadline.console.ConsoleProcess;
+import org.jboss.jreadline.console.settings.Settings;
+import org.jboss.jreadline.edit.actions.Operation;
+import org.jboss.jreadline.extensions.manual.Man;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:stale.pedersen@jboss.org">Ståle W. Pedersen</a>
+ */
+public class ExampleExtension {
+
+    public static void main(String[] args) throws IOException {
+
+        //Settings.getInstance().setAnsiConsole(false);
+        Settings.getInstance().setReadInputrc(false);
+       //Settings.getInstance().setHistoryDisabled(true);
+        //Settings.getInstance().setHistoryPersistent(false);
+        Console exampleConsole = new Console();
+
+        PrintWriter out = new PrintWriter(System.out);
+
+        Man man = new Man(exampleConsole);
+        man.addPage(new File("/tmp/README.md"), "test");
+
+
+        Completion completer = new Completion() {
+            @Override
+            public void complete(CompleteOperation co) {
+                // very simple completor
+                List<String> commands = new ArrayList<String>();
+                if(co.getBuffer().equals("fo") || co.getBuffer().equals("foo")) {
+                    commands.add("foo");
+                    commands.add("foobaa");
+                    commands.add("foobar");
+                    commands.add("foobaxxxxxx");
+                    commands.add("foobbx");
+                    commands.add("foobcx");
+                    commands.add("foobdx");
+                }
+                else if(co.getBuffer().equals("fooba")) {
+                    commands.add("foobaa");
+                    commands.add("foobar");
+                    commands.add("foobaxxxxxx");
+                }
+                else if(co.getBuffer().equals("foobar")) {
+                    commands.add("foobar");
+                }
+                else if(co.getBuffer().equals("bar")) {
+                    commands.add("bar/");
+                }
+                else if(co.getBuffer().equals("h")) {
+                    commands.add("help.history");
+                    commands.add("help");
+                }
+                else if(co.getBuffer().equals("help")) {
+                    commands.add("help.history");
+                    commands.add("help");
+                }
+                else if(co.getBuffer().equals("help.")) {
+                    commands.add("help.history");
+                }
+                else if(co.getBuffer().equals("deploy")) {
+                    commands.add("deploy /home/blabla/foo/bar/alkdfe/en/to/tre");
+                }
+                 co.setCompletionCandidates(commands);
+            }
+        };
+
+        exampleConsole.addCompletion(completer);
+        exampleConsole.addCompletion(man);
+
+        String line;
+        //console.pushToConsole(ANSIColors.GREEN_TEXT());
+        while ((line = exampleConsole.read("[test@foo.bar]~> ")) != null) {
+            exampleConsole.pushToConsole("======>\"" + line + "\"\n");
+
+            if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit") ||
+                    line.equalsIgnoreCase("reset")) {
+                break;
+            }
+            if(line.equalsIgnoreCase("password")) {
+                line = exampleConsole.read("password: ", Character.valueOf((char) 0));
+                exampleConsole.pushToConsole("password typed:" + line + "\n");
+
+            }
+            if(line.equals("clear"))
+                exampleConsole.clear();
+            if(line.startsWith("man")) {
+                //exampleConsole.attachProcess(test);
+                man.attach();
+                man.setCurrentManPage("test");
+            }
+        }
+        if(line.equals("reset")) {
+            exampleConsole.stop();
+            exampleConsole = new Console();
+
+            while ((line = exampleConsole.read("> ")) != null) {
+                exampleConsole.pushToConsole("======>\"" + line + "\"\n");
+                if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit") ||
+                        line.equalsIgnoreCase("reset")) {
+                    break;
+                }
+
+            }
+        }
+
+        try {
+            exampleConsole.stop();
+        } catch (Exception e) {
+        }
+    }
+}
---
+++ src/main/java/org/jboss/jreadline/extensions/manual/Man.java
@@ -0,0 +1,121 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.jreadline.extensions.manual;
+
+import org.jboss.jreadline.complete.CompleteOperation;
+import org.jboss.jreadline.complete.Completion;
+import org.jboss.jreadline.console.Config;
+import org.jboss.jreadline.console.Console;
+import org.jboss.jreadline.console.ConsoleProcess;
+import org.jboss.jreadline.edit.actions.Operation;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A Man implementation for JReadline. ref: http://en.wikipedia.org/wiki/Man_page
+ *
+ *
+ * @author <a href="mailto:stale.pedersen@jboss.org">Ståle W. Pedersen</a>
+ */
+public class Man extends ConsoleProcess implements Completion {
+
+    private int rows;
+    private int columns;
+    private int topVisibleRow;
+    private List<ManPage> manPages = new ArrayList<ManPage>();
+    private ManPage current;
+
+    public Man(Console console) {
+        super(console);
+        manPages = new ArrayList<ManPage>();
+    }
+
+    public void addPage(File file, String name) {
+        manPages.add(new ManPage(file, name));
+    }
+
+    @Override
+    protected void afterAttach() throws IOException {
+        console.switchToAlternateScreenBuffer();
+
+        rows = console.getTerminalHeight();
+        columns = console.getTerminalWidth();
+    }
+
+    @Override
+    protected void afterDetach() throws IOException {
+        console.switchToMainScreenBuffer();
+
+    }
+
+    public void setCurrentManPage(String name) throws IOException {
+        for(ManPage page : manPages) {
+            if(name.equals(page.getName()))
+                current = page;
+        }
+        current.loadPage();
+        topVisibleRow = 0;
+        displayMan();
+    }
+
+    @Override
+    public java.lang.String processOperation(Operation operation) throws IOException {
+        if(operation.getInput()[0] == 'q') {
+            detach();
+            return "";
+        }
+        else if(operation.getInput()[0] == 'j') {
+            topVisibleRow++;
+            displayMan();
+            return null;
+        }
+        else if(operation.getInput()[0] == 'k') {
+            if(topVisibleRow > 0)
+                topVisibleRow--;
+            displayMan();
+            return null;
+        }
+        else
+            return null;
+    }
+
+    private void displayMan() throws IOException {
+        console.clear();
+        for(int i=topVisibleRow; i < (topVisibleRow+rows); i++) {
+            console.pushToConsole(current.getLine(i));
+            console.pushToConsole(Config.getLineSeparator());
+        }
+    }
+
+    @Override
+    public void complete(CompleteOperation completeOperation) {
+        if(completeOperation.getBuffer().equals("m"))
+            completeOperation.getCompletionCandidates().add("man");
+        else if(completeOperation.getBuffer().equals("ma"))
+            completeOperation.getCompletionCandidates().add("man");
+        else if(completeOperation.getBuffer().equals("man"))
+            completeOperation.getCompletionCandidates().add("man");
+        else if(completeOperation.getBuffer().equals("man ")) {
+            for(ManPage page : manPages) {
+                completeOperation.getCompletionCandidates().add("man "+page.getName());
+            }
+        }
+    }
+}
