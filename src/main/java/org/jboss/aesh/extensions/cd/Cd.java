a2285787870684061b596f1743e8fb638dcc44a9
Daniel Cunha <danielsoro@gmail.com>
2014-05-14 23:24:52 -0300

    Create cd command

--- src/main/java/org/jboss/aesh/extensions/cd/Cd.java
+++ src/main/java/org/jboss/aesh/extensions/cd/Cd.java
@@ -1,16 +1,16 @@
 package org.jboss.aesh.extensions.cd;
 
-import org.jboss.aesh.cl.Arguments;
-import org.jboss.aesh.cl.CommandDefinition;
-import org.jboss.aesh.console.command.Command;
-import org.jboss.aesh.console.command.CommandResult;
-import org.jboss.aesh.console.command.invocation.CommandInvocation;
-import org.jboss.aesh.extensions.ls.Ls;
-import org.jboss.aesh.terminal.Shell;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
+import org.jboss.aesh.cl.Arguments;
+import org.jboss.aesh.cl.CommandDefinition;
+import org.jboss.aesh.cl.Option;
+import org.jboss.aesh.console.Config;
+import org.jboss.aesh.console.command.Command;
+import org.jboss.aesh.console.command.CommandResult;
+import org.jboss.aesh.console.command.invocation.CommandInvocation;
 
 /**
  * Use AeshConsole.getAeshContext().cwd as reference
@@ -20,19 +20,29 @@ import java.util.List;
 @CommandDefinition(name = "cd", description = "change directory [dir]")
 public class Cd implements Command {
 
+    @Option(shortName = 'h', name = "help", hasValue = false, description = "display this help and exit")
+    private boolean help;
+
     @Arguments
     private List<File> arguments;
 
     @Override
     public CommandResult execute(CommandInvocation commandInvocation) throws IOException {
-        if(arguments != null && arguments.size() > 0) {
+        if (help) {
+            commandInvocation.getShell().out().print(commandInvocation.getHelpInfo("cd"));
+            return CommandResult.SUCCESS;
+        }
 
-
+        if (arguments == null) {
+            updatePrompt(commandInvocation, new File(Config.getUserDir()));
+        }
+        else {
+            updatePrompt(commandInvocation, arguments.get(0));
         }
         return CommandResult.SUCCESS;
     }
 
-    public void updatePrompt(File file) {
-
+    private void updatePrompt(CommandInvocation commandInvocation, File file) {
+        commandInvocation.getAeshContext().setCurrentWorkingDirectory(file);
     }
 }
