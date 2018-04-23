90039a95c745906804306649112ccbd3c38348ff
Rostislav Svoboda <rsvoboda@redhat.com>
2018-04-23 14:18:36 +0200

    [AESH-461] Move of AeshExampleExtension from root

--- aesh/src/main/java/org/aesh/extensions/example/AeshExampleExtension.java
+++ aesh/src/main/java/org/aesh/extensions/example/AeshExampleExtension.java
@@ -1,4 +1,4 @@
-/*
+package org.aesh.extensions.example;/*
  * JBoss, Home of Professional Open Source
  * Copyright 2014 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @authors tag. All rights reserved.
